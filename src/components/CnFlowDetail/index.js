fdd39fbfb550ae2b6fc1a7e7ffe4e681bfb797dd
Ruben van der Linde <rubenvdlinde@gmail.com>
2026-08-03 14:36:35 +0200

    Revert "feat(flow): shared flow authoring components (#583)" (#584)
    This reverts commit 44516d40bf99bf36d83eeee778ecf5ba43ed9fa5.

--- src/components/CnFlowDetail/index.js
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: EUPL-1.2
-// Copyright (C) 2026 Conduction B.V.
-
-export { default as CnFlowDetail } from './CnFlowDetail.vue'
-export { default as CnFlowSidebar } from './CnFlowSidebar.vue'
-export { default as CnFlowIndexPage } from './CnFlowIndexPage.vue'
